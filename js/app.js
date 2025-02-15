27b6c7a0c738601c809e32f00230c842bc5cac56
Alfonso <alfonsorodriguez235@gmail.com>
2025-02-15 18:29:16 +0100

    perfil

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ document.getElementById("openModalRegister").addEventListener("click", function(
     document.getElementById("modalContainerRegister").style.display = "flex";
     document.body.style.overflow = "hidden";
 });
-document.getElementById("nextModal").addEventListener("click", function(){
+document.getElementById("nextModalPassword").addEventListener("click", function(){
   document.getElementById("modalContainerRegister").style.display="none";
   document.getElementById("modalContainerPassword").style.display="flex";
 })
+document.getElementById("nextModalDNI").addEventListener("click",function(){
+  document.getElementById("modalContainerPassword").style.display="none";
+  document.getElementById("modalContainerDNI").style.display="flex";
+})
+document.getElementById("nextModalSelfie").addEventListener("click",function(){
+  document.getElementById("modalContainerDNI").style.display="none";
+  document.getElementById("modalContainerSelfie").style.display="flex";
+})
 document.getElementById("closeModal").addEventListener("click", function() {
-    document.getElementById("modalContainerPassword").style.display = "none";
+    document.getElementById("modalContainerSelfie").style.display = "none";
     document.body.style.overflow = "auto";
+});
+document.getElementById("userIcon").addEventListener("click", function() {
+  window.location.href = "/html/perfil.html"; 
 });
